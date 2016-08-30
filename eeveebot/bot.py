28f1a859a0a6639e42d82eb16684591959de135a
Gabriel Soldani <soldani.gabriel@gmail.com>
2016-08-29 21:52:38 -0300

    Rename bot_client -> telegram_bot

--- eeveebot/bot.py
+++ eeveebot/bot.py
@@ -15,12 +15,12 @@ class BotThread(Thread):
     def __init__(self, app):
         super(BotThread, self).__init__()
         self.app = app
-        self.bot_client = telepot.Bot(args.telegram_key)
+        self.telegram_bot = telepot.Bot(args.telegram_key)
         
     def run(self):
         while True:
             try:
-                self.bot_client.message_loop(callback=self.on_message, run_forever=True)
+                self.telegram_bot.message_loop(callback=self.on_message, run_forever=True)
             except KeyboardInterrupt:
                 break
             except Exception as e:
@@ -34,7 +34,7 @@ class BotThread(Thread):
         user, created = User.get_or_create(chat_id=chat_id)
         
         if created:
-            self.bot_client.sendMessage(chat_id, 'Olá! Eu sou o Eevee Robot e eu posso te notificar dos Pokémons próximos!')
+            self.telegram_bot.sendMessage(chat_id, 'Olá! Eu sou o Eevee Robot e eu posso te notificar dos Pokémons próximos!')
         
         if 'location' in message:
             user.latitude = message['location']['latitude']
@@ -42,4 +42,4 @@ class BotThread(Thread):
             user.enabled = True
             user.save()
             
-            self.bot_client.sendMessage(chat_id, 'Localização atualizada. Assim que aparecer algum, eu vou te avisar :)')
+            self.telegram_bot.sendMessage(chat_id, 'Localização atualizada. Assim que aparecer algum Pokémon, eu vou te avisar :)')
